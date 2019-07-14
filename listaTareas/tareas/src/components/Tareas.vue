5dd8bff9791c3d68f17db6a84552135b86ebf58d
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-14 18:07:26 +0200

    cambio de color y de imagen con el cambio del estado de la tarea y carga las tareas desde un fichero externo

--- listaTareas/tareas/src/components/Tareas.vue
+++ listaTareas/tareas/src/components/Tareas.vue
@@ -10,14 +10,18 @@
 
     <div id="contenedor-listado-tareas" class="d-flex flex-row flex-wrap">
       <div
-        class="card"
+        class="card text-light"
         style="width: 18rem;"
         v-for="tarea of tareas"
         :key="tarea.id"
-        :class="{'bg-info' : tarea.completado , 'bg-danger' :!tarea.completado}"
+        :class="{'bg-success' : tarea.completado , 'bg-primary' :!tarea.completado}"
       >
-        <img
-          src="https://static01.nyt.com/images/2015/09/10/us/motherlode-homework-study/motherlode-homework-study-superJumbo.jpg"
+      
+        <img v-if="tarea.completado" :src="imagenTerminada"
+          class="card-img-top"
+          alt="..."
+        />
+        <img v-else :src="imagenPendiente"
           class="card-img-top"
           alt="..."
         />
@@ -25,7 +29,7 @@
           <h5 class="card-title">{{tarea.nombre}}</h5>
           <p class="card-text">{{tarea.descripcion}}</p>
           <a class="btn btn-danger" @click="borrado(tarea)">X</a>
-          <a class="btn btn-primary" @click="changeCompletado(tarea)">Completar</a>
+          <a class="btn btn-dark" @click="changeCompletado(tarea)">Completar</a>
         </div>
       </div>
     </div>
@@ -33,18 +37,18 @@
 </template>
 
 <script>
+import tareas from '../listadetareas.js'
 export default {
   data() {
     return {
       titulo: "Hola mundo con vue",
-      tareas: [
-        { nombre: "Crear panel", descripcion: "prueba", completado: false },
-        { nombre: "menu", descripcion: "prueba", completado: true },
-        { nombre: "contenido", descripcion: "prueba", completado: true }
-      ],
+      tareas: [...tareas],
       nuevatarea: "",
       descripcion: "",
-      completado: false
+      completado: false,
+      imagenPendiente:'https://static01.nyt.com/images/2015/09/10/us/motherlode-homework-study/motherlode-homework-study-superJumbo.jpg',
+      imagenTerminada:'https://tutorasap.com/wp-content/uploads/2015/01/yes-310x206.png',
+
     };
   },
   methods: {
---
+++ listaTareas/tareas/src/listadetareas.js
@@ -0,0 +1,8 @@
+export default [
+{ "nombre": "Crear panel", "descripcion": "crear panel de control", "completado": false },
+{ "nombre": "Crear menu", "descripcion": "Crear menu pagina del cliente xxxx", "completado": false },
+{ "nombre": "Diseño gráfico", "descripcion": "Primeros templates", "completado": true },
+{ "nombre": "Fix menu", "descripcion": "Solucionar menu tarea para ayer :)", "completado": false },
+{ "nombre": "Bocetos", "descripcion": "Realizar bocetos máxima urgencia", "completado": false },
+{ "nombre": "Breafing", "descripcion": "Recogida de requisitos del cliente nº 56776", "completado": false },
+{ "nombre": "Email", "descripcion": "Solucionar problemas con el correo del cliente", "completado": false },]
