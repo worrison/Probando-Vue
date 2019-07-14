e844a9b5e2cebf1ab6bfd69b6df3b264e76c9dba
victor ordaxm <victorordaxantolin@gmail.com>
2019-07-14 14:59:30 +0200

    añadir eliminar y actualizar el estado ok sin usar vuex

--- listaTareas/tareas/src/components/Tareas.vue
+++ listaTareas/tareas/src/components/Tareas.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
-    <input type="text" v-model="nuevaFruta" @keyup.enter="addTarea" />
-    <input type="number" v-model="cantidad" />
-    <span>Completado
-    <input type="checkbox" v-model="completado" name="" id=""></span>
+    <input type="text" v-model="nuevatarea" @keyup.enter="addTarea" />
+    <textarea v-model="descripcion" name id cols="50" rows="1"></textarea>
+    <span>
+      Completado
+      <input type="checkbox" v-model="completado" name id />
+    </span>
     <button @click="addTarea">Add</button>
 
     <div id="contenedor-listado-tareas" class="d-flex flex-row flex-wrap">
-          <div class="card" style="width: 18rem;" v-for="fruta of frutas" :key="fruta.id" :class="{'bg-info' : fruta.completado , 'bg-danger' :!fruta.completado}">
-            <img
-              src="https://static01.nyt.com/images/2015/09/10/us/motherlode-homework-study/motherlode-homework-study-superJumbo.jpg"
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">{{fruta.nombre}}</h5>
-              <p class="card-text">{{fruta.cantidad}}</p>
-              <a href="#" class="btn btn-primary">Completar</a>
-            </div>
-          </div>
-      
+      <div
+        class="card"
+        style="width: 18rem;"
+        v-for="tarea of tareas"
+        :key="tarea.id"
+        :class="{'bg-info' : tarea.completado , 'bg-danger' :!tarea.completado}"
+      >
+        <img
+          src="https://static01.nyt.com/images/2015/09/10/us/motherlode-homework-study/motherlode-homework-study-superJumbo.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{tarea.nombre}}</h5>
+          <p class="card-text">{{tarea.descripcion}}</p>
+          <a class="btn btn-danger" @click="borrado(tarea)">X</a>
+          <a class="btn btn-primary" @click="changeCompletado(tarea)">Completar</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,24 +37,38 @@ export default {
   data() {
     return {
       titulo: "Hola mundo con vue",
-      frutas: [
-        { nombre: "pera", cantidad: 10  , completado: false},
-        { nombre: "sandia", cantidad: 10 ,completado: true},
-        { nombre: "platano", cantidad: 10 , completado:true }
+      tareas: [
+        { nombre: "Crear panel", descripcion: "prueba", completado: false },
+        { nombre: "menu", descripcion: "prueba", completado: true },
+        { nombre: "contenido", descripcion: "prueba", completado: true }
       ],
-      nuevaFruta: "",
-      cantidad: 0
+      nuevatarea: "",
+      descripcion: "",
+      completado: false
     };
   },
   methods: {
     addTarea() {
-      this.frutas.push({
-        nombre: this.nuevaFruta,
-        cantidad: this.cantidad,
-        completado:this.completado,
+      this.tareas.push({
+        nombre: this.nuevatarea,
+        descripcion: this.descripcion,
+        completado: this.completado
       });
-      this.nuevaFruta = "";
+      this.nuevatarea = "";
+    },
+    borrado(tarea) {
+      let index = this.tareas.indexOf(tarea);
+      this.tareas.splice(index, 1);
+    },
+    changeCompletado(tarea) {
+      let index = this.tareas.indexOf(tarea);
+      if (this.tareas[index].completado) {
+        this.tareas[index].completado = false;
+      } else {
+        this.tareas[index].completado = true;
+      }
     }
-  }
+  },
+  computed: {}
 };
 </script>
